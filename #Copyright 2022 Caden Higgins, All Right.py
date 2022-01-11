5a25a4c49d525a11f21d9561fe0432e57b023b2e
Caden <Caden.k.higgins@gmail.com>
2022-01-11 09:25:20 -0800

    Create #Copyright 2022 Caden Higgins, All Right.py

---
+++ #Copyright 2022 Caden Higgins, All Right.py
@@ -0,0 +1,3 @@
+# Copyright 2022 Caden Higgins, All Rights Reserved
+name = input("Enter your name: ")
+print(f"Happy birthday to you, Happy birthday to you, Happy birthday dear {name}, Happy Birthday to you")
