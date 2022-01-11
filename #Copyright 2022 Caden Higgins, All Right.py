e41a7da6fd2035ce3de261d4c4893e1f06d908c1
Caden <Caden.k.higgins@gmail.com>
2022-01-11 09:35:31 -0800

    Update #Copyright 2022 Caden Higgins, All Right.py

--- #Copyright 2022 Caden Higgins, All Right.py
+++ #Copyright 2022 Caden Higgins, All Right.py
@@ -1,3 +1,5 @@
 # Copyright 2022 Caden Higgins, All Rights Reserved
 name = input("Enter your name: ")
+# This varible fetcehs the name from the user for the song
 print(f"Happy birthday to you, Happy birthday to you, Happy birthday dear {name}, Happy Birthday to you")
+# This line actually sings the song
