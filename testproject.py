209500b6f8908ac2c778f1ac2aeff79927ef3c89
Caden <Caden.k.higgins@gmail.com>
2022-01-14 08:48:45 -0800

    Update testproject.py

--- testproject.py
+++ testproject.py
@@ -1,5 +1,6 @@
 import math
 number = int( input ("pick a number between one and ten "))
+count = int(input("How many times would you like me to print your number? "))
 if number > 10:
     print("you idiot, the number is supposed to be between one and ten")
     exit
@@ -7,6 +8,6 @@ if number < 1:
     print("you idiot, the number is supposed to be between one and ten")
 else:
     print("good job you can follow directions")
-    print("I will now print your number 10 times")
-    for i in range(10):
+    print(f"I will now print your number {count} times")
+    for i in range(count):
         print(number)
