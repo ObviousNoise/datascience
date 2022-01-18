1ca18991c75bc9ee8738bdc16ed522ca6310d1d0
Caden <Caden.k.higgins@gmail.com>
2022-01-18 11:45:32 -0800

    Update #Slot Machine For Underaged Gambling.py

--- #Slot Machine For Underaged Gambling.py
+++ #Slot Machine For Underaged Gambling.py
@@ -1,1 +1,6 @@
 #Slot Machine For Underaged Gambling
+underage = input("are you of the legal gambling age in your state")
+if underage == ("no"):
+    print ("Come back when you're older")
+if underage == ("yes"):    
+#Slot program goes here
