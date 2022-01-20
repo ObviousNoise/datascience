90266a7d8f30f6c85411223143ca149582205992
Caden <Caden.k.higgins@gmail.com>
2022-01-20 10:02:45 -0800

    commit

--- #Slot Machine For Underaged Gambling.py
+++ #Slot Machine For Underaged Gambling.py
@@ -1,17 +1,6 @@
 #Slot Machine For Underaged Gambling
-import math
-import random
-import time
-
-#Actual code starts here
-underage = input("are you of the legal gambling age in your state? ")
+underage = input("are you of the legal gambling age in your state")
 if underage == ("no"):
     print ("Come back when you're older")
-if underage == ("yes"):
-    #this just says this for meemes
-    print("shall we play a game?")
-    print
-    
-else:
-    print("I dont understand, please try again.")
-    exit
+if underage == ("yes"):    
+#Slot program goes here 
